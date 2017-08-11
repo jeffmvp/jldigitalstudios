574d7ac3db67d4572ccebe3a25347915f54d67b9
Jeff Leslie <lesli052@gmail.com>
2017-08-10 20:38:47 -0500

    reset :(

--- src/client/components/header.jsx
+++ src/client/components/header.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Link} from 'react-router';
-import '../css/header';
 
 export default class Header extends React.Component {
 
@@ -15,29 +14,22 @@ export default class Header extends React.Component {
 	showLoginModal = () => {
 		this.props.showLoginModal(true);
 	}
-	animateAndScroll(e) {
-		$('html, body').animate({
-			scrollTop: $(e.target.id).offset().top
-		}, 1000);
-	}
 
 	render() {
 		return(
-			<div>
-				<div className="Header">
-					<div className="Header-container">
-						<div class="grid">
-							<div className="grid_col grid_col--5-of-5">
-								<ul>
-									<Link to="/">Home</Link>
-									<a id="#travel" onClick={this.animateAndScroll} className="btn btn-primary btn-space-col btn-large-size">Services</a>
-									<a id="#manager" onClick={this.animateAndScroll} className="btn btn-primary btn-large-size btn-space-col">Industries</a>
-									<a id="#host" onClick={this.animateAndScroll} className="btn btn-primary btn-large-size btn-space-col">Contact</a>
-								</ul>
-						</div>
-						</div>
+			<div className="header">
+				<Link to="/" className="headerItem logo">YourCompany.com</Link>
+				<nav className="headerItem navContainer">
+					<div className="navItem">
+						<Link className={location.pathname === "/" ? "selected" : ""} to="/host">Learn more</Link>
 					</div>
-				</div>
+					<div className="navItem">
+						<a>Sign up</a>
+					</div>
+					<div className="navItem">
+						<a onClick={this.showLoginModal}>Log in</a>
+					</div>
+				</nav>
 			</div>
 		)
 	}
